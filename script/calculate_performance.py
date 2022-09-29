82155cf12ce28fa83cbbb0315327162db9b04436
Krisztian Szabo <developer.krisz@gmail.com>
2022-09-29 12:34:26 +0200

    added calculation of confusion matrix

--- script/calculate_performance.py
+++ script/calculate_performance.py
@@ -1,14 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-import pandas as pd
 from pathlib import Path
-from sklearn import svm, datasets
-# from sklearn.model_selection import train_test_split
-from sklearn.metrics import ConfusionMatrixDisplay
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+# import matplotlib.pyplot as plt
 
-# import some data to play with
-# iris = datasets.load_iris()
+# from sklearn import svm, datasets
+# from sklearn.svm import SVC
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import ConfusionMatrixDisplay
 
 def handle_running_from_different_directories(file_name: str):
     cwd = Path.cwd()
@@ -17,50 +16,31 @@ def handle_running_from_different_directories(file_name: str):
         return cwd / base
     elif (cwd / ".." / base).exists():
         return cwd / ".." / base
-    # print(csv_loc)
-# exit()
 
-file_name = "humandata_modified.csv"
-csv_loc = handle_running_from_different_directories(file_name)
-
+file_name_ground_truth = "merge_result.csv"
+csv_loc = handle_running_from_different_directories(file_name_ground_truth)
 df = pd.read_csv(csv_loc)
-print(df)
-exit()
+
+# create cutoffs csv
+df_2 = df[["human", "humanDetectron", "score"]].copy()
+df_2["score"] = df_2["score"].round(decimals = 3)
+df_2.rename(columns={"human": "ground_truth", "humanDetectron": "detectron-all"}, inplace=True, errors="raise")
+
+df_2["detectron-0.5"] = df_2["detectron-all"].where(df_2["score"] > 0.5, 0).tolist()
+df_2["detectron-0.6"] = df_2["detectron-all"].where(df_2["score"] > 0.6, 0).tolist()
+df_2["detectron-0.7"] = df_2["detectron-all"].where(df_2["score"] > 0.7, 0).tolist()
+df_2["detectron-0.8"] = df_2["detectron-all"].where(df_2["score"] > 0.8, 0).tolist()
+
+df_2.to_csv("./csv/cutoffs.csv")
 
 
+# Calculating Confusion matrix
+y = df["human"] # definite truth
 
-## working on it
+for conf_level in np.arange(0.5, 0.85, 0.05):
+    conf_level = round(conf_level,2)
+    X = df["humanDetectron"].where(df['score'] > conf_level, 0)
+    # continue
+    tn, fp, fn, tp = confusion_matrix(y, X).ravel()
+    print(f"\n\tcutoff conf lvl: {conf_level}\n tn:{tn}\n fp:{fp}\n fn:{fn}\n tp:{tp}")
 
-X = iris.data
-y = iris.target
-class_names = iris.target_names
-
-# Split the data into a training set and a test set
-# X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-# Run classifier, using a model that is too regularized (C too low) to see
-# the impact on the results
-# classifier = svm.SVC(kernel="linear", C=0.01).fit(X_train, y_train)
-
-# np.set_printoptions(precision=2)
-
-# Plot non-normalized confusion matrix
-titles_options = [
-    ("Confusion matrix, without normalization", None),
-    ("Normalized confusion matrix", "true"),
-]
-for title, normalize in titles_options:
-    disp = ConfusionMatrixDisplay.from_estimator(
-        classifier,
-        X_test,
-        y_test,
-        display_labels=class_names,
-        cmap=plt.cm.Blues,
-        normalize=normalize,
-    )
-    disp.ax_.set_title(title)
-
-    print(title)
-    print(disp.confusion_matrix)
-
-plt.show()
