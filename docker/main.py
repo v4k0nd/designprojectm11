cc179f55d5c8538e0d1f23814af42189bc616331
Krisztian Szabo <developer.krisz@gmail.com>
2022-10-02 16:46:10 +0200

    updated Dockerfile to include python-multipart for multiple file upload

--- docker/main.py
+++ docker/main.py
@@ -1,15 +1,37 @@
-from typing import Union
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
+@app.post("/files/")
+async def create_files(
+    files: List[bytes] = File(description="Multiple files as bytes"),
+):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/v1/analyse/")
+async def create_upload_files(
+    files: List[UploadFile] = File(description="Multiple files as UploadFile"),
+):
+    return {"filenames": [file.filename for file in files]}
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/v1/analyse/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
