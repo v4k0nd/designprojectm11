cd84c28269c1e665681fbd5879c9e862bdb3f1ad
Krisztián Szabó <k.szabo@student.utwente.nl>
2023-07-20 10:58:35 +0200

    trying to log gpus

--- demo/demo.py
+++ demo/demo.py
@@ -105,19 +105,20 @@ if __name__ == "__main__":
     
     # Check if CUDA is available
     if torch.cuda.is_available():
-        # Log the current device
+        logger.info(f"===== GPU info =====")
         curr_dev_nr = torch.cuda.current_device()
         logger.info(f"Current device: {torch.cuda.current_device()}")
         
-        # Log the number of GPUs
         logger.info(f"Number of GPUs: {torch.cuda.device_count()}")
         
-        # Log the name of the current device
         logger.info(f"Current device name: {torch.cuda.get_device_name(curr_dev_nr)}")
+
+        total_memory = torch.cuda.get_device_properties(curr_dev_nr).total_memory
+        logger.info(f"Total memory on device {curr_dev_nr}: {total_memory}")
         
-        # Log the memory allocation and cache for the current device
         logger.info(f"Memory allocated: {torch.cuda.memory_allocated(curr_dev_nr)}")
-        logger.info(f"Memory cached: {torch.cuda.memory_cached(curr_dev_nr)}")
+        logger.info(f"Memory cached (reserved): {torch.cuda.memory_reserved(curr_dev_nr)}")
+        logger.info("====================")
     else:
         logger.info("No GPUs detected.")
 
